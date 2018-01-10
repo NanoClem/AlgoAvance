3d8357f84c3bcb800b829b2616d4b8859eee7a1d
DAMIENS Lucas <d16008614@A-F002591-L.salsa.univ-amu.fr>
2018-01-10 17:02:28 +0100

    Lol

---
+++ Huffman/Tree.hpp
@@ -0,0 +1,159 @@
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+#include "Node.hpp"
+
+template <class T> class Node;
+
+
+template<class T>
+class Tree {
+
+	private:
+		Node<T> *root;
+		void Insert(Node<T> *);
+		void Scan(Node<T> *);
+		inline Node<T> *Root() const {return root;}
+		Node<T> *Search(const T) const;
+
+	public:
+		inline Tree() {root = nullptr;}
+		~Tree();
+		void Create(const T, const int);
+		void Delete(const T);
+		void Viewing();
+		inline void NbrNodes() {cout << Node<T>::NbNode;}
+};
+
+
+template <class T>
+void Tree<T>::Viewing()
+{
+	if(root!=nullptr)
+		Scan(root);
+	else
+		cout <<  "L'arbre est vide" << endl;
+}
+
+
+template<class T>
+void Tree<T>::Scan(Node<T> *pNode)
+{
+	if(pNode->left)
+		Scan(pNode->left);
+
+	if(pNode)
+		cout << pNode->nodeValue <<":"<<pNode->frequence << "/n";
+
+	if(pNode->right)
+		Scan(pNode->right);
+}
+
+
+template<class T>
+void Tree<T>::Create(const T val, const int freq)
+{
+	Node<T> * pTmpNode = new Node<T>;
+
+	pTmpNode->left = nullptr;
+	pTmpNode->right = nullptr;
+
+	pTmpNode->nodeValue = val;
+	pTmpNode->frequence = freq;
+
+	Insert(pTmpNode);
+}
+
+
+template <class T>
+void Tree<T>::Insert(Node <T> *pNode)
+{
+	if(root == nullptr)
+	{
+		root=pNode;
+		return;
+	}
+
+	Node<T> * current = root;
+	Node<T> * previous = nullptr;
+
+	while(current)
+	{
+		previous=current;
+
+		if(pNode->frequence < current->frequence)
+			current = current->left;
+		else
+			current = current->right;
+	}
+
+	if(pNode->frequence < previous->frequence)
+		previous->left = pNode;
+	else
+		previous->right = pNode;
+}
+
+template<class T>
+void Tree<T>::Delete(const int freq)
+{
+	Node<T> *pNode = Search(freq);
+
+	if(!pNode)
+	{
+		cout << "Recherche ratée, cette valeur ne se trouve pas dans l'arbre"<<endl;
+		return;
+	}
+
+	Node<T> *right = pNode->right;
+	Node<T> *left = pNode->left;
+	Node<T> *current = root;
+
+	if (pNode == root)
+	{
+		root = right;
+
+		if (left) Insert(left);
+
+		delete pNode;
+		return;
+	}
+
+	while(current)
+	{
+		if (current->right == pNode || current->left == pNode) break;
+
+		if (pNode->frequence >= current->frequence)
+			current = current->droite;
+		else
+			current = current->gauche;
+	}
+
+	if (current->right == pNode)
+		current->right = right;
+	else
+		current->left = right;
+
+	if (left) Insert(left);
+
+	delete pNode;
+	cout <<"Noeud effacé!"<<endl;
+}
+
+
+template <class T>
+Node<T> *Tree<T>::Search(const int freq)const
+{
+	Node<T> *current = root;
+
+	while(current)
+	{
+		if (freq < current->frequence)
+			current = current->left;
+		else
+			current = current->right;
+	}
+
+	return nullptr;
+}
